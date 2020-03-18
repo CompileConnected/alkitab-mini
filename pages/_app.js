052a77495b42260627b1b34fb19d45f774a7e094
Yohanes Kustiadi <yokho95@gmail.com>
2020-03-18 10:32:55 +0700

    minor improvement

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,7 @@ import App from 'next/app';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
+
 
 export default class MyApp extends App {
   componentDidMount() {
@@ -19,7 +15,6 @@ export default class MyApp extends App {
   }
 
   render() {
-    const [value, setValue] = useState(0);
     const { Component, pageProps } = this.props;
 
     return (
@@ -28,17 +23,6 @@ export default class MyApp extends App {
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
           <CssBaseline />
           <Component {...pageProps} />
-            <BottomNavigation
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-              }}
-              showLabels
-            >
-              <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-              <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-              <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-          </BottomNavigation>
         </ThemeProvider>
       </React.Fragment>
     );
